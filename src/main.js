8df95145ebe03a5a98567c8db2a5024e406e06df
mikolas <mikolas.valousek.024@pslib.cz>
2025-09-11 13:28:08 +0200

    x

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import javascriptLogo from './javascript.svg';
 import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './style.css';

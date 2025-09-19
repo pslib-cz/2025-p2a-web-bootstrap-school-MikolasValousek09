1ebe007efa881f4870d4767622590da5a6ffa4c6
mikoláš.V. <mikolas.valousek.024@pslib.cz>
2025-09-19 12:50:21 +0200

    initial

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import javascriptLogo from './javascript.svg';
+import viteLogo from '/vite.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './style.css';
+
+
